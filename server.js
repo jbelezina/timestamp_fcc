47842f7b4ee6b3b6c2f7bfdc819e5a86d706cff6
Jan Belezina <jbelezina@gmail.com>
2016-10-31 18:27:53 +0000

    Added main logic and fixed a nice ejs homepage

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var app = express();
+"use strict";
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    parse = require('parse-date/silent'),
+    app = express();
+    
+app.use(bodyParser.urlencoded());
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+
+  res.render('home');
+});
+
+app.get('/:timestamp', function (req, res) {
+  
+  let passedIn = req.params.timestamp,
+      isNumber = /^[0-9.]+$/.test(passedIn),
+      result;
+  
+  if (isNumber) {
+    var convertedToInt = parseInt(passedIn);
+    result = parse(convertedToInt);
+  } else {
+    result = parse(passedIn);
+  }
+  
+  if (result instanceof Date) {
+    
+    let unix = result.getTime(),
+        locale = "en-us",
+        month = result.toLocaleString(locale, { month: "long" }),
+        day = result.getDate(),
+        year = result.getFullYear(),
+        natural = month + " " + day + ", " + year;
+    
+    res.json({
+      "unix" : unix,
+      "natural" : natural
+    });
+  } else {
+     res.json({
+      "unix" : null,
+      "natural" : null
+    });
+  }
+  
 });
 
 app.listen(8080, function () {
